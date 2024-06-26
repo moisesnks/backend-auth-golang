8aa53231707567c71904d688c5de285d0b6642a2
moisesnks <mleiva@utem.cl>
2024-06-25 23:16:11 -0400

    swag docs

--- docs/docs.go
+++ docs/docs.go
@@ -601,7 +601,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8081",
+	Host:             "https://backend-autch.tssw.cl",
 	BasePath:         "/",
 	Schemes:          []string{"http"},
 	Title:            "Backend API",
